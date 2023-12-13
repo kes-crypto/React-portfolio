ad07e76bd3cceb8dfbc16c484e0142f557344647
Dennis Kestone <denniskestone@gmail.com>
2023-12-14 02:46:50 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -126,7 +126,7 @@ export default function Home() {
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <h3 className="text-3xl py-1 dark:text-white ">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet earum similique dicta voluptates consequuntur id cum tempore. Nisi optio, nemo, deleniti corporis maxime qui libero aperiam laborum officiis quod quidem.
             </p>
