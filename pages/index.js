bdd495c983d59fb32551da39b9ea7e179ea8bac3
Dennis Kestone <denniskestone@gmail.com>
2023-12-14 02:38:48 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,16 +74,10 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet earum similique dicta voluptates consequuntur id cum tempore. Nisi optio, nemo, deleniti corporis maxime qui libero aperiam laborum officiis quod quidem.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet earum similique dicta voluptates consequuntur id cum tempore. Nisi optio, nemo, deleniti corporis maxime qui libero aperiam laborum officiis quod quidem.
             </p>
           </div>
           <div className="lg:flex gap-10">
